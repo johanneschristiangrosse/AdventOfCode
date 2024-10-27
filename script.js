58d114ecdc45848edc8f07c4da4dbcb825551cfa
Johannes Große <johannes.christian.grosse@gmail.com>
2024-10-28 00:49:09 +0100

    add issue two of days

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-import { issue2015_12_01 } from "./lib/issues/2015_12_01.js";
+import { issue2015_12_01_issue_1 } from "./lib/issues/2015_12_01.js";
 
 document.addEventListener("DOMContentLoaded", start)
 
 const issues = []
-issues['2015_12_01'] = issue2015_12_01
+issues['2015_12_01_issue_1'] = issue2015_12_01_issue_1
 
 function start() {
   const issueList = document.getElementById('issues')
@@ -14,47 +14,78 @@ function start() {
 }
 
 function getYearControl(year) {
-  const yearControl = document.createElement('li')
-  yearControl.appendChild(getYearLinkControl(year))
-  yearControl.appendChild(getDayListControl(year))
-  return yearControl
+  const control = document.createElement('li')
+  control.classList.add('contents')
+  control.appendChild(getYearLinkControl(year))
+  control.appendChild(getDayListControl(year))
+  return control
 }
 
 function getYearLinkControl(year) {
-  const yearLinkControl = document.createElement('a')
-  yearLinkControl.innerText = `${year}`
-  yearLinkControl.href = `https://adventofcode.com/${year}`
-  yearLinkControl.setAttribute('target', '_blank')
-  return yearLinkControl
+  const linkControl = document.createElement('a')
+  linkControl.innerText = `${year}`
+  linkControl.href = `https://adventofcode.com/${year}`
+  linkControl.setAttribute('target', '_blank')
+
+  const control = document.createElement('h2')
+  control.appendChild(linkControl)
+  return control
 }
 
 function getDayListControl(year) {
-  const dayListControl = document.createElement('ul')
+  const control = document.createElement('ul')
+  control.classList.add('contents')
 
   for (let day = 1; day <= 25; day++) {
-    dayListControl.appendChild(getDayControl(year, day))
+    control.appendChild(getDayControl(year, day))
   }
 
-  return dayListControl
+  return control
 }
 
 function getDayControl(year, day) {
-  const dayControl = document.createElement('li')
-  dayControl.appendChild(getDayLinkControl(year, day))
-  dayControl.appendChild(getDayResultControl(year, day))
-  return dayControl
+  const control = document.createElement('li')
+  control.classList.add('contents')
+  control.appendChild(getDayStringControl(year, day))
+  control.appendChild(getIssueOneLinkControl(year, day))
+  control.appendChild(getIssueOneResultControl(year, day))
+  control.appendChild(getIssueTwoLinkControl(year, day))
+  control.appendChild(getIssueTwoResultControl(year, day))
+  return control
 }
 
-function getDayLinkControl(year, day) {
-  const dayLinkControl = document.createElement('a')
-  dayLinkControl.innerText = `${day.toString().padStart(2, '0')}.12.${year}`
-  dayLinkControl.href = `https://adventofcode.com/${year}/day/${day}`
-  dayLinkControl.setAttribute('target', '_blank')
-  return dayLinkControl
+function getDayStringControl(year, day) {
+  const control = document.createElement('span')
+  control.innerText = `${day.toString().padStart(2, '0')}.12.${year}`
+  return control
 }
 
-function getDayResultControl(year, day) {
-  const issueKey = `${year}_12_${day.toString().padStart(2, '0')}`
-  const resultString = issues[issueKey] ? `: ${issues[issueKey]()}` : ''
-  return document.createTextNode(resultString)
+function getIssueOneLinkControl(year, day) {
+  const control = document.createElement('a')
+  control.innerText = `Issue 1:`
+  control.href = `https://adventofcode.com/${year}/day/${day}`
+  control.setAttribute('target', '_blank')
+  return control
+}
+
+function getIssueOneResultControl(year, day) {
+  const control = document.createElement('span')
+  const issueKey = `${year}_12_${day.toString().padStart(2, '0')}_issue_1`
+  control.innerText = issues[issueKey] ? issues[issueKey]() : ''
+  return control
+}
+
+function getIssueTwoLinkControl(year, day) {
+  const control = document.createElement('a')
+  control.innerText = `Issue 2:`
+  control.href = `https://adventofcode.com/${year}/day/${day}#part2`
+  control.setAttribute('target', '_blank')
+  return control
+}
+
+function getIssueTwoResultControl(year, day) {
+  const control = document.createElement('span')
+  const issueKey = `${year}_12_${day.toString().padStart(2, '0')}_issue_2`
+  control.innerText = issues[issueKey] ? issues[issueKey]() : ''
+  return control
 }
