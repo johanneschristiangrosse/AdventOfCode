5f797a5f3da80f53edc47e17d3d18cea9c427424
Johannes Große <johannes.christian.grosse@gmail.com>
2024-10-28 01:50:32 +0100

    solve day_2015_12_02_issue_1

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 import { day_2015_12_01_issue_1 } from "./lib/issues/day_2015_12_01_issue_1.js";
 import { day_2015_12_01_issue_2 } from "./lib/issues/day_2015_12_01_issue_2.js";
+import { day_2015_12_02_issue_1 } from "./lib/issues/day_2015_12_02_issue_1.js";
 
 document.addEventListener("DOMContentLoaded", start)
 
 const issues = []
 issues['day_2015_12_01_issue_1'] = day_2015_12_01_issue_1
 issues['day_2015_12_01_issue_2'] = day_2015_12_01_issue_2
+issues['day_2015_12_02_issue_1'] = day_2015_12_02_issue_1
 
 function start() {
   const issueList = document.getElementById('issues')
