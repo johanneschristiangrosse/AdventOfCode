840037cc2d92a1a8ceec798be779982defb54129
Johannes Große <johannes.christian.grosse@gmail.com>
2024-10-29 12:54:38 +0100

    solve day_2015_12_03_issue_2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import { day_2015_12_01_issue_2 } from "./lib/issues/day_2015_12_01_issue_2.js";
 import { day_2015_12_02_issue_1 } from "./lib/issues/day_2015_12_02_issue_1.js";
 import { day_2015_12_02_issue_2 } from "./lib/issues/day_2015_12_02_issue_2.js";
 import { day_2015_12_03_issue_1 } from "./lib/issues/day_2015_12_03_issue_1.js";
+import { day_2015_12_03_issue_2 } from "./lib/issues/day_2015_12_03_issue_2.js";
 
 document.addEventListener("DOMContentLoaded", start)
 
@@ -12,6 +13,7 @@ issues['day_2015_12_01_issue_2'] = day_2015_12_01_issue_2
 issues['day_2015_12_02_issue_1'] = day_2015_12_02_issue_1
 issues['day_2015_12_02_issue_2'] = day_2015_12_02_issue_2
 issues['day_2015_12_03_issue_1'] = day_2015_12_03_issue_1
+issues['day_2015_12_03_issue_2'] = day_2015_12_03_issue_2
 
 function start() {
   const issueList = document.getElementById('issues')
